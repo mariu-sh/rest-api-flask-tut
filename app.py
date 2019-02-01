68c8225a4036694dffc9c6ae782566902aceded8
MariuszF <mariusz.figiel@mailfence.com>
2019-02-01 20:58:19 +0100

    Changes in settings

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import getenv
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -12,7 +13,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 
 # DATABASE CONFIG
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.before_first_request
